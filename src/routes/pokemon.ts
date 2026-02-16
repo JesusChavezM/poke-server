e91642d55db1474b106dfd0426c2ebcd33662065
Jesús Chavez <chavezmele7030@gmail.com>
2026-02-16 02:44:25 -0600

    fix: correct import path for getPokemonByName function and add getPokemonList endpoint

--- src/routes/pokemon.ts
+++ src/routes/pokemon.ts
@@ -1,8 +1,10 @@
-import { getPokemonByName } from '@/api/pokemon';
+import { getPokemonByName } from '@/api/getPokemonByname';
+import { getPokemonList } from '@/api/getPokemonList';
 import { Router } from 'express';
 
 const router = Router();
 // TODO: implement other API routes (e.g. list, create, update, delete)
+router.get('/', getPokemonList);
 router.get('/:name', getPokemonByName);
 
 export default router;
