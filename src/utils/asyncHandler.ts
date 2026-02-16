0def61fe985c3d6cd77f63f91923e663c8c770ee
Jesús Chavez <chavezmele7030@gmail.com>
2026-02-16 02:12:58 -0600

    add asyncHandler utility for handling asynchronous route handlers

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+import type { RequestHandler } from 'express';
+
+export const asyncHandler = (
+  fn: (req: any, res: any, next: any) => Promise<any>
+): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
