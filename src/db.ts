0a38aaf627645a63946002f71d8c024356e5a229
Jesús Chavez <chavezmele7030@gmail.com>
2026-02-17 22:34:59 -0600

    add temporal logs to watch mongo connection

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,12 @@ import { MONGO_URI } from './config';
 
 export async function connectDb() {
   try {
-    await mongoose.connect(MONGO_URI, {});
+    await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      connectTimeoutMS: 30000,
+      maxPoolSize: 5,
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error', err);
